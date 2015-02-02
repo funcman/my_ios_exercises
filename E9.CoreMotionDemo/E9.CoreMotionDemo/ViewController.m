f4ee0a2cfd934c3d67845c8f71a59db5d3ff7283
funcman <hyq1986@gmail.com>
2015-02-02 18:37:29 +0800

    implementation code

--- E9.CoreMotionDemo/E9.CoreMotionDemo/ViewController.m
+++ E9.CoreMotionDemo/E9.CoreMotionDemo/ViewController.m
@@ -1,6 +1,13 @@
 #import "ViewController.h"
+#import <CoreMotion/CoreMotion.h>
 
-@interface ViewController ()
+@interface ViewController () {
+    CMStepCounter*  stepCounter;
+    CMPedometer*    pedometer;
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *numOfSteps;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *meter;
 
 @end
 
@@ -9,11 +16,55 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.meter.selectedSegmentIndex = 0;
+    [self installStepCounter];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)switchMeter:(id)sender {
+    self.numOfSteps.text = @"0";
+    switch (self.meter.selectedSegmentIndex) {
+        case 0:
+            [self installStepCounter];
+            break;
+        case 1:
+            [self installPedometer];
+            break;
+        default:
+            break;
+    }
+}
+
+- (void)installStepCounter {
+    NSLog(@"Use CMStepCounter.");
+    [pedometer stopPedometerUpdates];
+    stepCounter = [[CMStepCounter alloc]init];
+    [stepCounter startStepCountingUpdatesToQueue:[NSOperationQueue mainQueue] updateOn:0 withHandler:^(NSInteger numberOfSteps, NSDate* timestamp, NSError* error) {
+        if (!error) {
+            [self showNum:[NSNumber numberWithInteger:numberOfSteps]];
+            NSLog(@"CMStepCounter update: %@ steps now.", self.numOfSteps.text);
+        }
+    }];
+}
+
+- (void)installPedometer {
+    NSLog(@"Use CMPedometer.");
+    [stepCounter stopStepCountingUpdates];
+    pedometer = [[CMPedometer alloc]init];
+    [pedometer startPedometerUpdatesFromDate:[NSDate date] withHandler:^(CMPedometerData* data, NSError* error){
+        if (!error) {
+            [self performSelectorOnMainThread:@selector(showNum:) withObject:data.numberOfSteps waitUntilDone:YES];
+            NSLog(@"CMPedometer update: %@ steps now.", self.numOfSteps.text);
+        }
+    }];
+}
+
+- (void)showNum:(NSNumber*)number {
+    self.numOfSteps.text = [number stringValue];
 }
 
 @end
