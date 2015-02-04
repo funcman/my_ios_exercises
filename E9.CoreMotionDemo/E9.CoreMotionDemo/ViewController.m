c1803c0e6b1c24d3eaae819fa817ee68d4ace0a3
funcman <hyq1986@gmail.com>
2015-02-04 16:17:42 +0800

    add motion activity manager

--- E9.CoreMotionDemo/E9.CoreMotionDemo/ViewController.m
+++ E9.CoreMotionDemo/E9.CoreMotionDemo/ViewController.m
@@ -2,11 +2,13 @@
 #import <CoreMotion/CoreMotion.h>
 
 @interface ViewController () {
-    CMStepCounter*  stepCounter;
-    CMPedometer*    pedometer;
+    CMStepCounter*              stepCounter;
+    CMPedometer*                pedometer;
+    CMMotionActivityManager*    activityManager;
 }
 
 @property (weak, nonatomic) IBOutlet UILabel *numOfSteps;
+@property (weak, nonatomic) IBOutlet UILabel *activity;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *meter;
 @property (weak, nonatomic) IBOutlet UITextView *logView;
 
@@ -18,8 +20,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [self.logView.layer setCornerRadius:3];
+
     self.meter.selectedSegmentIndex = 0;
     [self installStepCounter];
+
+    [self installActivityManager];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -60,7 +65,32 @@
     [pedometer startPedometerUpdatesFromDate:[NSDate date] withHandler:^(CMPedometerData* data, NSError* error){
         if (!error) {
             [self performSelectorOnMainThread:@selector(showNum:) withObject:data.numberOfSteps waitUntilDone:YES];
-            [self appendLog:[NSString stringWithFormat:@"Pedometer update: %@ steps now.", self.numOfSteps.text]];        }
+            [self appendLog:[NSString stringWithFormat:@"Pedometer update: %@ steps now.", self.numOfSteps.text]];
+        }
+    }];
+}
+
+- (void)installActivityManager {
+    [self appendLog:@"CMActivityManager start."];
+    activityManager = [[CMMotionActivityManager alloc]init];
+    [activityManager startActivityUpdatesToQueue:[NSOperationQueue mainQueue] withHandler:^(CMMotionActivity* activity) {
+        if (activity.stationary) {
+            self.activity.text = @"Stationary";
+        }else if (activity.walking) {
+            self.activity.text = @"Walking";
+        }else if (activity.running) {
+            self.activity.text = @"Running";
+        }else if (activity.automotive) {
+            self.activity.text = @"Automotive";
+        }else if (activity.cycling) {
+            self.activity.text = @"Cycling";
+        }else {
+            self.activity.text = @"Unknown";
+        }
+        [self appendLog:[NSString stringWithFormat:@"Activity:%@ StartTime:%@ Confidence:%d",
+                         self.activity.text,
+                         [self getTime:activity.startDate],
+                         (int)activity.confidence]];
     }];
 }
 
@@ -70,12 +100,16 @@
 
 - (void)appendLog:(NSString*)log {
     dispatch_async(dispatch_get_main_queue(), ^{
-        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
-        [dateFormatter setDateFormat:@"HH:mm:ss:SSS"];
-        NSString *curTimeStr = [dateFormatter stringFromDate:[NSDate date]];
+        NSString* curTimeStr = [self getTime:[NSDate date]];
         NSLog(@"%@", log);
         self.logView.text = [NSString stringWithFormat:@"%@: %@\n%@", curTimeStr, log, self.logView.text];
     });
+}
+
+- (NSString*)getTime:(NSDate*)date {
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
+    [dateFormatter setDateFormat:@"HH:mm:ss:SSS"];
+    return [dateFormatter stringFromDate:date];
 }
 
 @end
